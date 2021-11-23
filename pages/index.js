519094ed2cc8c0f31b62e3df45bbe02baefe41fc
TRECN <rishabh1103@outlook.com>
2021-11-23 21:54:25 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ export default function Home() {
         <title>Instagram</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <h1>this is an instagram build</h1>
         {/*header*/}
         {/*feed*/}
         {/*Model*/}
