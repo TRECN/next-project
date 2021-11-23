8bff759d0eb208b244cd40281e92201b1e616de8
TRECN <rishabh1103@outlook.com>
2021-11-23 18:45:03 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,11 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Instagram</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
         <h1>this is an instagram build</h1>
+
     </div>
   )
 }
