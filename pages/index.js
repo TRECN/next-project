297e55a3aec4783178ed787139e2d99f8aff1ffc
TRECN <rishabh1103@outlook.com>
2021-11-23 21:54:00 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,16 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    
+    <div className="">
+      <Head>
+        <title>Instagram</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+        <h1>this is an instagram build</h1>
+        {/*header*/}
+        {/*feed*/}
+        {/*Model*/}
+
+    </div>
   )
 }
