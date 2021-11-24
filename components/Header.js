961e6653ad7717eda3ad155a575a6063a51b1975
TRECN <rishabh1103@outlook.com>
2021-11-24 16:44:22 +0530

    commit

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,13 @@
+import Image from 'next/image'
 function Header() {
     return (
         <div>
             <h1>This is the header</h1> 
 
             {/* left */}
+            <div>
+                <Image/>
+            </div>
             {/* middle */}
             {/* right */}
 
