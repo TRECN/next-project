77c3cdf8a56aa43e0af8bdbeff7b14656a70b792
TRECN <rishabh1103@outlook.com>
2021-11-24 16:40:51 +0530

    commit

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,11 @@ function Header() {
     return (
         <div>
             <h1>This is the header</h1> 
+
+            {/* left */}
+            {/* middle */}
+            {/* right */}
+
         </div>
     )
 }
