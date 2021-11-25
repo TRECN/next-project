dde79e2ea900be652dcf80ba0f16049f236dcbef
TRECN <rishabh1103@outlook.com>
2021-11-25 21:09:03 +0530

    commit

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,10 @@
 import Image from 'next/image'
 import {
     SearchIcon,
-
+    PlusCircleIcon,
+    UserGroupIcon,
+    HeartIcon,
+    PaperAirplaneIcon,
     MenuIcon
 } from '@heroicons/react/outline'
 import {HomeIcon} from '@heroicons/react/solid'
