ab2e8b48fc608a33a80218e5cbed9133ede8b0ae
TRECN <rishabh1103@outlook.com>
2021-11-26 22:14:50 +0530

    commit

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ import {
 import {HomeIcon} from '@heroicons/react/solid'
 function Header() {
     return (
-        <div className='shadow-sn border-b bg-white sticky top-0'>
+        <div className='shadow-sn border-b bg-white sticky top-0 z-10'>
             <div className='flex justify-between max-w-6xl mx-10 lg:mx-auto '>
                 {/* left - logo*/}
                 <div className='relative hidden lg:inline-grid w-24'>
