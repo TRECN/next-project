e7ef32ffe57474410f8316f9ab068398cfc88663
TRECN <rishabh1103@outlook.com>
2021-11-28 19:24:23 +0530

    commit

--- components/Stories.js
+++ components/Stories.js
@@ -16,7 +16,7 @@ function Stories() {
     return (
         <div>
             {suggestions.map(profile=>(
-               <Story key={profile.id}/> 
+               <Story key={profile.id} img={profile.avatar} username={profile.username}/> 
             ))}
             {/* stories */}
 
