031d1a838654850b1f737e3a917dbfdfa2747b8c
TRECN <rishabh1103@outlook.com>
2021-11-28 19:13:21 +0530

    commit

--- components/Stories.js
+++ components/Stories.js
@@ -4,9 +4,9 @@ function Stories() {
 
     useEffect(()=>{
         const Suggestions = [...Array(20)].map((_,i)=>({
-
-        })
-        )
+            ...faker.helpers.contextualCard(),
+            id: i,
+        }))
     },[])
 
     return (
