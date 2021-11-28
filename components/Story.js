579311b7b113e34f8ab95eb18a455956a26c73da
TRECN <rishabh1103@outlook.com>
2021-11-28 23:02:32 +0530

    commit

--- components/Story.js
+++ components/Story.js
@@ -4,7 +4,7 @@ function Story({img, username}) {
         <div >
            <div className='relative w-20'>
                <Image
-                src={img}
+                src='https://media-exp1.licdn.com/dms/image/C5603AQF8Ig-OMxb8ng/profile-displayphoto-shrink_200_200/0/1618815249650?e=1639612800&v=beta&t=QBWC4GzCZYL7LwJToeQwqN9yIsF_CArs6nvjcSqVfn4'
                 layout='fill'
                 objectFit='contain'
                />
