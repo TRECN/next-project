6714b3da96e17900919632948492777ae43f9487
TRECN <rishabh1103@outlook.com>
2021-11-28 19:17:48 +0530

    commit

--- components/Stories.js
+++ components/Stories.js
@@ -2,13 +2,14 @@ import faker from 'faker'
 import { useEffect, useState } from 'react'
 function Stories() {
 
-    const [suggestions,setSuggestions]=useState();
+    const [suggestions,setSuggestions]=useState([]);
 
     useEffect(()=>{
         const Suggestions = [...Array(20)].map((_,i)=>({
             ...faker.helpers.contextualCard(),
             id: i,
         }))
+        setSuggestions(Suggestions)
         console.log(Suggestions)
     },[])
 
