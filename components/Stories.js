69e5598cddd3ed691ff7619b4c147beafc98fcf0
TRECN <rishabh1103@outlook.com>
2021-11-30 22:29:03 +0530

    commit

--- components/Stories.js
+++ components/Stories.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import Story from './Story'
 function Stories() {
 
+    const [suggestions,setSuggestions]=useState([]);
 
     useEffect(()=>{
         const suggestions = [...Array(5)].map((_,i)=>({
