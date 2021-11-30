be9de19b7122bff259375f9efc828ede21353578
TRECN <rishabh1103@outlook.com>
2021-11-30 22:27:09 +0530

    commit

--- components/Story.js
+++ components/Story.js
@@ -3,7 +3,11 @@ function Story({img, username}) {
     return (
         <div >
            <div className='relative w-10 h-10'>
-               
+               <Image
+                src={img}
+                layout='fill'
+                objectFit='contain'
+               />
            </div>
            <p>{username}</p>
         </div>
