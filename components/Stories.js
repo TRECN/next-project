ccddfcdca0bcb23ed16223211944c21f21a5f0f4
TRECN <rishabh1103@outlook.com>
2021-12-05 21:52:50 +0530

    commit

--- components/Stories.js
+++ components/Stories.js
@@ -5,14 +5,7 @@ function Stories() {
 
     const [suggestions,setSuggestions]=useState([]);
 
-    useEffect(()=>{
-        const suggestions = [...Array(5)].map((_,i)=>({
-            ...faker.helpers.contextualCard(),
-            id: i,
-        }))
-        setSuggestions(suggestions)
-        console.log(suggestions)
-    },[])
+    
 
     return (
         <div className='flex space-x-2 p-6 bg-white mt-8 border-gray-200'>
