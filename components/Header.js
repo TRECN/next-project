0558bbba7a07800b64a2c42c1d0d59244985b6bd
TRECN <rishabh1103@outlook.com>
2021-12-05 21:59:12 +0530

    commit

--- components/Header.js
+++ components/Header.js
@@ -43,13 +43,7 @@ function Header() {
                 <div className='flex items-center justify-end space-x-4 '>
                     <HomeIcon className='navBtn'/>
                     <MenuIcon className='h-6 md:hidden cursor-pointer'/>
-                    <div className='relative navBtn -top-1'>
-                        <PaperAirplaneIcon className='rotate-45'/>
-                        <div className='absolute -top-1 -right-2 rounded-full 
-                        text-xs w-5 h-5 bg-red-500 flex items-center justify-center animate-pulse
-                        text-white'>3
-                        </div>
-                    </div>
+                    
                     <PlusCircleIcon className='navBtn'/>
                     <UserGroupIcon className='navBtn'/>
                     <HeartIcon className='navBtn'/>
