1508866c6f51d9181ddf02d78507ac82142556ea
TRECN <rishabh1103@outlook.com>
2021-12-05 21:56:21 +0530

    commit

--- components/Header.js
+++ components/Header.js
@@ -11,16 +11,7 @@ import {HomeIcon} from '@heroicons/react/solid'
 function Header() {
     return (
         <div className='shadow-sn border-b bg-white sticky top-0 z-10'>
-            <div className='flex justify-between max-w-6xl mx-10 lg:mx-auto '>
-                {/* left - logo*/}
-                <div className='relative hidden lg:inline-grid w-24'>
-                    {/* Image is a next.js tag  */}
-                    <Image
-                    src="https://links.papareact.com/ocw" 
-                    layout="fill"
-                    objectFit='contain'
-                    />
-                </div>
+            
                 <div className='relative w-10 lg:hidden flex-shrink-0
                 cursor-pointer'>
                     <Image
