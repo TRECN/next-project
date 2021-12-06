d78fda06af0ff70a63e128dfbc04503770a38cef
TRECN <rishabh1103@outlook.com>
2021-12-06 22:48:12 +0530

    commit

--- components/Feed.js
+++ components/Feed.js
@@ -1,4 +1,8 @@
- <main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl 
+import Stories from "./Stories"
+
+function Feed() {
+    return (
+        <main className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl 
         xl:grid-cols-3 xl:max-w-6xl mx-auto'>
             {/* mx-auto is used to center all */}
             {/* Section Left*/}
