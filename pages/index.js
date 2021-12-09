eed435f08c503b2e1df22e109c2bb56c6ca48486
TRECN <rishabh1103@outlook.com>
2021-12-09 22:17:10 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,21 @@
-i
+import Head from 'next/head'
+import Feed from '../components/Feed'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div className="">
+      <Head>
+        <title>Instagram</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+        {/*header*/}
+        <Header/>
+        {/*feed*/}
+        <Feed/>
+
+        {/*Model*/}
+
+    </div>
+  )
+}
