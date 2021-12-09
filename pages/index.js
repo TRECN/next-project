f86a894e9da674d92f429dc03ab7ad8f185c15c3
TRECN <rishabh1103@outlook.com>
2021-12-09 22:13:54 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import Feed from '../components/Feed'
 import Header from '../components/Header'
 
 export default function Home() {
-
+  return (
+    <div className="">
       <Head>
         <title>Instagram</title>
         <link rel="icon" href="/favicon.ico" />
