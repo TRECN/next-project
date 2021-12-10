af327375c2e26ef8a91dca29863c1f6176505099
TRECN <rishabh1103@outlook.com>
2021-12-10 21:20:05 +0530

    commit

--- components/Story.js
+++ components/Story.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 function Story({img, username}) {
     return (
         <div >
-           <div className='relative w-14 h-14'>
+           <div className='relative w-14 h-14 rounded-full p-[1.5]'>
                <Image
                 src={img}
                 layout='fill'
