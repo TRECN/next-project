5d965473d169ce9954fdfe12c9177ff8f6a09832
TRECN <rishabh1103@outlook.com>
2021-12-12 22:54:18 +0530

    commit

--- components/Header.js
+++ components/Header.js
@@ -13,22 +13,7 @@ function Header() {
         <div className='shadow-sn border-b bg-white sticky top-0 z-10'>
             <div className='flex justify-between max-w-6xl mx-10 lg:mx-auto '>
                 {/* left - logo*/}
-                <div className='relative hidden lg:inline-grid w-24'>
-                    {/* Image is a next.js tag  */}
-                    <Image
-                    src="https://links.papareact.com/ocw" 
-                    layout="fill"
-                    objectFit='contain'
-                    />
-                </div>
-                <div className='relative w-10 lg:hidden flex-shrink-0
-                cursor-pointer'>
-                    <Image
-                    src="https://links.papareact.com/jjm" 
-                    layout="fill"
-                    objectFit='contain'
-                    />
-                </div>
+                
                 {/* middle - search input field*/}
                 <div className='max-w-xs '>
                     <div className='relative mt-1 p-3 rounded-md '>
