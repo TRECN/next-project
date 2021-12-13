667e701cf884de63b3a317ccff22cd8bb40e6500
TRECN <rishabh1103@outlook.com>
2021-12-13 21:45:31 +0530

    commit

--- components/Header.js
+++ components/Header.js
@@ -30,12 +30,26 @@ function Header() {
                     />
                 </div>
                 {/* middle - search input field*/}
-                
+                <div className='max-w-xs '>
+                    <div className='relative mt-1 p-3 rounded-md '>
+                        <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
+                            <SearchIcon className='h-5 w-5 text-gray-400'/>
+                        </div>
+                        <input className='bg-gray-50 block w-full pl-10 sm:text-sm
+                        border-gray-300 focus:border-black focus: ring-black rounded-md' type="text" placeholder="Search" />
+                    </div>
+                </div>
                 {/* right */}
                 <div className='flex items-center justify-end space-x-4 '>
                     <HomeIcon className='navBtn'/>
                     <MenuIcon className='h-6 md:hidden cursor-pointer'/>
-                    
+                    <div className='relative navBtn -top-1'>
+                        <PaperAirplaneIcon className='rotate-45'/>
+                        <div className='absolute -top-1 -right-2 rounded-full 
+                        text-xs w-5 h-5 bg-red-500 flex items-center justify-center animate-pulse
+                        text-white'>3
+                        </div>
+                    </div>
                     <PlusCircleIcon className='navBtn'/>
                     <UserGroupIcon className='navBtn'/>
                     <HeartIcon className='navBtn'/>
