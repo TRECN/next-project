5fa09bcc450d988892dba20ba217d3c1fc1b0fd5
TRECN <rishabh1103@outlook.com>
2021-12-14 22:36:01 +0530

    commit

--- components/Header.js
+++ components/Header.js
@@ -35,7 +35,9 @@ function Header() {
                         <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
                             <SearchIcon className='h-5 w-5 text-gray-400'/>
                         </div>
-                        
+                        <input className='bg-gray-50 block w-full pl-10 sm:text-sm
+                        border-gray-300 focus:border-black focus: ring-black rounded-md' type="text" placeholder="Search" />
+                    </div>
                 </div>
                 {/* right */}
                 <div className='flex items-center justify-end space-x-4 '>
