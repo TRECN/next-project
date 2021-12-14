837ab7ab10ab3dc052527fbe1f81d73382f6ecc9
TRECN <rishabh1103@outlook.com>
2021-12-14 22:38:49 +0530

    commit

--- components/Stories.js
+++ components/Stories.js
@@ -18,7 +18,9 @@ function Stories() {
         <div className='flex space-x-2 p-6 bg-white mt-8 border-gray-200
         border rounded-sm overflow-x-scroll'>
             {suggestions.map((profile)=>(
-              
+               <Story 
+               key={profile.id} 
+               
             
         </div>
     )
